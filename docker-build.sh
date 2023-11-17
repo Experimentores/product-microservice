5390d77670f00a1de27d76acaf567a39da641bd0
Davila Yoimer <89022117+Yoimer-Davila@users.noreply.github.com>
2023-11-17 01:39:54 -0500

    updated compose config

--- docker-build.sh
+++ docker-build.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
-docker rmi tripstore-products-service
-docker build . -t tripstore-products-service
+name=tripstore-products-service
+docker rmi "$name"
+docker build . -t "$name"
